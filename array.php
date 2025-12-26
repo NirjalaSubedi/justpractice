7483736996b3371cf8a40cb48794ac68da345ef2
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-26 06:51:39 +0545

    Add initial array handling and display functionality

---
+++ array.php
@@ -0,0 +1,22 @@
+<?php
+    $arraydata=array("html","css","js","php");
+    var_dump($arraydata);
+    $age=array("nirjala"=>22,"sujan"=>24,"ram"=>23);
+    echo"<br>";
+    var_dump($age);
+    foreach($age as $key=>$value){
+        echo"<br>";
+        echo "$key $value";
+    }
+
+    $faculty=array(
+        "nirjala"=>"BCA",
+        "samikshya"=>"na padhne",
+    );
+    echo"<br>";
+    foreach($faculty as $key=>$value){
+        echo"<br>";
+        echo "$key : $value";
+    }
+    
+?>
