b266c4371d3c364c549722f2594957bba565371a
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-26 07:19:12 +0545

    Add form validation and styling, along with database connection setup

--- array.php
+++ array.php
@@ -19,4 +19,5 @@
         echo "$key : $value";
     }
     
+    
 ?>
---
+++ assocativeArray.php
@@ -0,0 +1,16 @@
+<?php
+$emp=[[1,"nirjala","bca",22],
+[2,"sujan","bim",24],
+[3,"ram","bba",23]
+];
+
+foreach($emp as $row){
+    foreach($row as $data){
+        echo $data." ";
+    }
+    echo"<br>";
+}
+
+$multidiminationalArray=[
+
+];
