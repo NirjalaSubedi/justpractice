5c65cc25881627df05fa8ee0c1103ed6b4b4dd0e
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-26 07:53:17 +0545

    data insertion

--- formvalidation/data.php
+++ formvalidation/data.php
@@ -1,3 +1,13 @@
 <?php
- print_r($_POST);
+include 'dbConnect.php';
+// print_r($_POST);
+$username=$_POST['username'];
+$email=$_POST['email'];
+$age=$_POST['age'];
+$sql="INSERT INTO `user` (`username`,`email`,`age`) VALUES ('$username','$email','$age')";
+if(mysqli_query($conn,$sql)){
+    echo"New record inserted";
+}else{
+    echo"Error:".$sql."<br>".mysqli_error($conn);
+}
 ?>
